37ff06f05f5c49ffc36c456c76d3f4ecafce29c5
Josh Owens <me@jowens.dev>
2021-03-06 13:38:55 -0600

    Adding in links to Welcome page

--- stubs/inertia/resources/js/Pages/Welcome.jsx
+++ stubs/inertia/resources/js/Pages/Welcome.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { InertiaLink } from '@inertiajs/inertia-react';
 
-export default function Welcome({ canLogin, canRegister, laravelVersion, phpVersion, ...props }) {
-    console.log({ props })
+function Links({ user, canRegister }) {
+    if (user) {
+        return (
+            <InertiaLink href="/dashboard" className="text-sm text-gray-700 underline">
+                Dashboard
+            </InertiaLink>
+        );
+    }
+
+    return (
+        <>
+            <InertiaLink href={route('login')} className="text-sm text-gray-700 underline">
+               Login
+            </InertiaLink>
+            {canRegister && (<InertiaLink href={route('register')} className="ml-4 text-sm text-gray-700 underline">
+                Register
+            </InertiaLink>)}
+        </>
+    );
+}
+
+export default function Welcome({ canLogin, canRegister, laravelVersion, phpVersion, user }) {
     return (
         <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             {canLogin && (
-                <InertiaLink href="/dashboard" className="text-sm text-gray-700 underline">
-                    Dashboard
-                </InertiaLink>
+                <Links user={user} canRegister={canRegister} />
             )}
         </div>
     );
