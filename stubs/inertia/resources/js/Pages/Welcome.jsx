0bc7c2946e2eb5416e3572c4c1106e6e97462f1a
Josh Owens <me@jowens.dev>
2021-03-06 12:09:15 -0600

    ADding in inertia link to dashboard

--- stubs/inertia/resources/js/Pages/Welcome.jsx
+++ stubs/inertia/resources/js/Pages/Welcome.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { InertiaLink } from '@inertiajs/inertia-react';
 
-export default function Welcome({ canLogin, canRegister, laravelVersion, phpVersion }) {
+export default function Welcome({ canLogin, canRegister, laravelVersion, phpVersion, ...props }) {
+    console.log({ props })
     return (
         <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             {canLogin && (
-                <div />
+                <InertiaLink href="/dashboard" className="text-sm text-gray-700 underline">
+                    Dashboard
+                </InertiaLink>
             )}
         </div>
     );
