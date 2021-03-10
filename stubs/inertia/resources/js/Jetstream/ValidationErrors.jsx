b65708a689b3a9523d1ea9a75b84de3a02c68ac4
Josh Owens <me@jowens.dev>
2021-03-09 19:59:32 -0600

    Starting to build out registration form

--- stubs/inertia/resources/js/Jetstream/ValidationErrors.jsx
+++ stubs/inertia/resources/js/Jetstream/ValidationErrors.jsx
@@ -4,7 +4,7 @@ import { nanoid } from 'nanoid';
 export default function ValidationErrors({ errors }) {
     if (Object.keys(errors).length > 0) {
         return (
-            <div>
+            <div className="mb-4">
                 <div className="font-medium text-red-600">Whoops! Something went wrong.</div>
 
                 <ul className="mt-3 list-disc list-inside text-sm text-red-600">
--- stubs/inertia/resources/js/Pages/Auth/Register.jsx
+++ stubs/inertia/resources/js/Pages/Auth/Register.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from '@inertiajs/inertia-react';
+import AuthenticationCard from '@/Jetstream/AuthenticationCard';
+import ValidationErrors from '@/Jetstream/ValidationErrors';
 
 export default function Register() {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+        terms: false,
+    });
+
+    function submit(e) {
+        e.preventDefault()
+        post(route('register'), {
+            onFinish: () => reset('password', 'password_confirmation')
+        });
+    }
+
     return (
-        <div id="register"></div>
+        <AuthenticationCard>
+            <ValidationErrors errors={errors} />
+        </AuthenticationCard>
     );
 }
