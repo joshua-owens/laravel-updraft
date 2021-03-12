5e0412f6564585118bea80da2a6e5449750f3889
Josh Owens <me@jowens.dev>
2021-03-11 19:33:44 -0600

    setting up application mark

--- stubs/inertia/resources/js/Layouts/AppLayout.jsx
+++ stubs/inertia/resources/js/Layouts/AppLayout.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Banner from '@/Jetstream/Banner'
+import ApplicationMark from '@/Jetstream/ApplicationMark'
+import { InertiaLink  } from '@inertiajs/inertia-react';
 
 export default function AppLayout({ children }) {
     return (
         <div>
             <Banner />
+            <div className="min-h-screen bg-gray-100">
+                <nav className="bg-white border-b border-gray-100">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="flex justify-between h-16">
+                            <div className="flex">
+                                <div className="flex-shrink-0 flex items-center">
+                                    <InertiaLink href={route('dashboard')}>
+                                        <ApplicationMark className="block h-9 w-auto" />
+                                    </InertiaLink>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
             {children}
         </div>
     );
